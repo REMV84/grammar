51a98275c62409c8b26c56dc2dbbaa207ffb3a1c
Ron <rhon.ventura@gmail.com>
2019-11-05 00:01:08 +0000

    Destory grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   
   def index
   end 
@@ -39,6 +39,15 @@ class GramsController < ApplicationController
       return render :edit, status: :unprocessable_entity
     end 
   end
+
+  def destroy
+     @gram = Gram.find_by_id(params[:id])
+     return render_not_found if @gram.blank?
+
+     @gram.destroy
+    redirect_to root_path
+  end
+
   private
 
   def gram_params
